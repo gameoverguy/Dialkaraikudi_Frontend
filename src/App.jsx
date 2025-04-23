246bc5f0e926953cb85b9a713da66ed2e6c03937
Jeeva <jeeva.digitaly@gmail.com>
2025-04-23 15:41:46 +0530

    login and details

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import BusinessDetails from "./business/BusinessDetails";
+import BusinessLogin from "./business/login";
+
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <BusinessLogin />
+      <BusinessDetails/>
     </>
   );
 }
