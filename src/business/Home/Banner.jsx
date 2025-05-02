adea9f5716baa51fdb1b966a0bb8c2e9c77c5e3f
unknown <sakthi.digitaly@gmail.com>
2025-05-02 16:26:31 +0530

    02-05-2025

--- src/business/Home/Banner.jsx
+++ src/business/Home/Banner.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import homebanner from "../../assets/home-banner.jpg";
+import React from 'react';
+import homebanner from "../../assets/home-banner.webp";
 
 function Banner() {
   return (
-    
-      <div
-        className="flex bg-cover justify-center w-full items-center md:bg-auto md:bg-center md:w-full opacity-90 lg:py-5">
-            <img src={homebanner} alt='' className='w-full h-[30vh] lg:h-[50vh] object-fit-cover' />
-      </div>
-    
-  )
+    <div className="w-full px-4 py-4 lg:px-40 lg:py-5">
+      <img
+        src={homebanner}
+        alt="Home Banner"
+        className="w-full h-[30vh] sm:h-[35vh] md:h-[40vh] lg:h-[25vh] object-fit-cover rounded-md"
+      />
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
