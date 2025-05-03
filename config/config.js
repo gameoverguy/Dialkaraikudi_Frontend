3cbc8f1b68b1d413b829cf59bf80d43f72208b7b
gameoverguy <118584840+gameoverguy@users.noreply.github.com>
2025-05-03 10:13:29 +0530

    03_05

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
-export const API = "http://192.168.1.33:5000";
+//export const API = "http://192.168.1.33:5000";
 
-// export const API = "http://localhost:5000";
+export const API = "http://localhost:5000";
 
 // export const API = "https://dialkaraikudi-backend.onrender.com";
