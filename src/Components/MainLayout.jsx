13ffe13c5fb3e7c199e14dd6a380f2e79a26d8b4
gameoverguy <118584840+gameoverguy@users.noreply.github.com>
2025-05-03 14:59:58 +0530

    02_05

--- src/Components/MainLayout.jsx
+++ src/Components/MainLayout.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import Header from './Header'
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer'
+import React from "react";
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 
 const MainLayout = () => {
-    return (
-        <div className='w-full mx-auto'>
-            <Header />
-            <div className='min-h-127 w-full'>
-                <Outlet />
-            </div>
-            <Footer />
+  return (
+    <>
+      <div className="w-full mx-auto">
+        <Header />
+        <div className="min-h-127 w-full">
+          <Outlet />
         </div>
-    )
-}
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-export default MainLayout
+export default MainLayout;
