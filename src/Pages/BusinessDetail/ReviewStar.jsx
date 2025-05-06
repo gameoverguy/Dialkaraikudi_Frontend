04f3c9b6b7bf61d25485b4f0a8e23cee24fd2a97
Varshizzz <kalaivarshi19@gmail.com>
2025-05-06 18:45:17 +0530

    ...

--- src/Pages/BusinessDetail/ReviewStar.jsx
+++ src/Pages/BusinessDetail/ReviewStar.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import { FaStar } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
+import { useLoginModal } from '../../context/LoginContext';
 
 const StarRating = ({ formData, businessId }) => {
     const [rating, setRating] = useState(0);
     const [hover, setHover] = useState(0);
     const navigate = useNavigate();
+    const userData = JSON.parse(sessionStorage.getItem("userData"));
+    const isLoggedin = !!userData;
+    const { handleOpenLoginModal } = useLoginModal();
 
     const handleRatingClick = (value) => {
+        if (!isLoggedin) {
+            handleOpenLoginModal();
+            return;
+        }
+
         setRating(value);
         navigate('/review', {
             state: {
@@ -26,8 +35,8 @@ const StarRating = ({ formData, businessId }) => {
                     <FaStar
                         key={index}
                         className={`cursor-pointer text-2xl transition-colors duration-200 ${starValue <= (hover || rating)
-                                ? 'text-yellow-400'
-                                : 'text-gray-300'
+                            ? 'text-yellow-400'
+                            : 'text-gray-300'
                             }`}
                         onClick={() => handleRatingClick(starValue)}
                         onMouseEnter={() => setHover(starValue)}
