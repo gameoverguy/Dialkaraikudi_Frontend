c5339659d83a79f73fd690e8fe2b993625282ef8
gameoverguy <118584840+gameoverguy@users.noreply.github.com>
2025-05-07 15:40:27 +0530

    07_05

--- src/business/Home/TopProducts.jsx
+++ src/business/Home/TopProducts.jsx
@@ -7,13 +7,13 @@ function TopProducts() {
       <div className="w-full md:w-11/12 flex flex-col md:flex-row justify-center items-center mx-auto cursor-pointer py-5">
         <div className="w-full md:w-6/12 relative group overflow-hidden">
           <video
-                    className="w-full h-[340px] object-cover"
-                    src="https://res.cloudinary.com/dstm2ouer/video/upload/v1746612083/store_nh16ay.mp4" // Replace with your actual video path
-                    autoPlay
-                    muted
-                    loop
-                    playsInline
-                  />
+            className="w-full h-[340px] object-cover"
+            src="https://res.cloudinary.com/dstm2ouer/video/upload/v1746612083/store_nh16ay.mp4" // Replace with your actual video path
+            autoPlay
+            muted
+            loop
+            playsInline
+          />
           {/* Overlay */}
           <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-40 transition-opacity duration-300"></div>
           {/* Centered Text */}
