dfd68a672da2d6ca786f51786adab4ddce9152db
gameoverguy <118584840+gameoverguy@users.noreply.github.com>
2025-05-09 13:17:35 +0530

    09_05

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="751339214-dfk3l174fjauuaa42hlje1er5civkres.apps.googleusercontent.com">
-    <App />
-    </GoogleOAuthProvider>;
-  </StrictMode>,
-)
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>
+);
