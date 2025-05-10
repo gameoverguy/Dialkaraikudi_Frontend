98076ee712d50bcca6197897c3c066409add3ab1
Abdul_Dialkkdi <abdul.dialkkdi@email.com>
2025-05-10 10:25:11 +0530

    sunscription

--- src/Components/AdminRoute.jsx
+++ src/Components/AdminRoute.jsx
@@ -1,26 +1,21 @@
 import { Navigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
-export const AdminRoute = ({ children }) => {
+const AdminRoute = ({ children }) => {
   const adminToken = Cookies.get('adminToken');
-  console.log(adminToken);
+  const businessToken = Cookies.get('businessToken');
   
   // const isDirectAccess = !document.referrer.includes(window.location.host);
 
   if ( !adminToken) {
     return <Navigate to="/adminlogin" replace />;
-  } return children;
-};
-
-
-
-export const VendorRoute = ({ children }) => {
-  const businessToken = Cookies.get('businessToken');
-  console.log(businessToken);
-  
-  // const isDirectAccess = !document.referrer.includes(window.location.host);
-
-  if ( !businessToken) {
+  }
+  else if (!businessToken) {
     return <Navigate to="/" replace />;
-  } return children;
+  }
+
+
+  return children;
 };
+
+export default AdminRoute;
--- src/Pages/VendorPanel/Index.jsx
+++ src/Pages/VendorPanel/Index.jsx
@@ -214,6 +214,7 @@ const VendorPanel = () => {
     // Clear admin token
     Cookies.remove("businessToken");
     // Navigate to admin login
+    localStorage.setItem("selectedMenuItem", "1");
     navigate("/");
     // toast.success('Logout successful');
   };
