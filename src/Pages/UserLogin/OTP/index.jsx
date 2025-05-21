affc289339845accea0582f52a328f494f967907
Varshizzz <kalaivarshi19@gmail.com>
2025-05-21 12:36:07 +0530

    ...

--- src/Pages/UserLogin/OTP/index.jsx
+++ src/Pages/UserLogin/OTP/index.jsx
@@ -15,7 +15,7 @@ const OTP = ({
 }) => {
   const [otp, setOtp] = useState(["", "", "", ""]);
   const [error, setError] = useState("");
-  const [timer, setTimer] = useState(60);
+  const [timer, setTimer] = useState(120);
   const [errorOverall, setErrorOverall] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [successMessage, setSuccessMessage] = useState("");
@@ -93,7 +93,7 @@ const OTP = ({
         otp: otpValue,
       });
       console.log(role);
-      console.log(response.data);
+      console.log(endpoint, response.data, "with endpoint");
 
       if (response.data) {
         console.log("OTP Verified:", otpValue);
@@ -136,7 +136,7 @@ const OTP = ({
   const handleResendOTP = async () => {
     try {
       const endpoint = isSignupFlow
-        ? `${API}/user/resendSignupOtp` // You'll need to create this endpoint
+        ? `${API}/user/resendregisterotp` // You'll need to create this endpoint
         : role === "business"
         ? `${API}/business/forgotPassword`
         : `${API}/user/forgotpassword`;
@@ -148,7 +148,7 @@ const OTP = ({
         setOtp(["", "", "", ""]);
         setError("");
         setErrorOverall("");
-        setTimer(60);
+        setTimer(120);
         setSuccessMessage("OTP resent successfully!");
         setTimeout(() => setSuccessMessage(""), 3000);
       }
